7e3750e7803fca887def1065ca78d50faff43641
MadsRF <madsrunefrederiksen@gmail.com>
2020-04-16 11:26:10 +0200

    working on U.S C1

--- public/itemPage/itemPage.js
+++ public/itemPage/itemPage.js
@@ -1,41 +1,43 @@
+$(document).ready( () => {
 
-  $(document).ready( () =>{
-
-    // dummy data over items - C0 story - 15-05-2020
-    let items = [
-        { id: "1", NAME: "Black Coffee", PRICE: 20},
-        { id: "2", NAME: "Cappuccino", PRICE: 25},
-        { id: "3", NAME: "Kaffe Latte", PRICE: 30},
-        { id: "4", NAME: "Hot Chocolate", PRICE: 38},
-        { id: "5", NAME: "Filter Kaffe", PRICE: 10},
-        { id: "6", NAME: "Water", PRICE: 1230}
-      ];
-    
+  // dummy data over items - C0 story - 15-05-2020
+  let items = [
+      { id: "1", NAME: "Black Coffee", PRICE: 20},
+      { id: "2", NAME: "Cappuccino", PRICE: 25},
+      { id: "3", NAME: "Kaffe Latte", PRICE: 30},
+      { id: "4", NAME: "Hot Chocolate", PRICE: 38},
+      { id: "5", NAME: "Filter Kaffe", PRICE: 10},
+      { id: "6", NAME: "Water", PRICE: 1230}
+    ];
   
-    $.each(items, function () {
-        
-        $(`<div id = item-box>
-            Id:          ${this.id}<br>
-            Name:        ${this.NAME}<br>
-            Price:       ${this.PRICE}<br><br>`
-        ).appendTo("#item-box");        
-    });
-
-    //USER STORY C1 - 15-05-2020
   
-    $('#addItem-button').on('click',function () {
-
-      var newItem = {
-          id : 20,
-          NAME : $("#name").val(),
-          PRICE : $("#price").val(),
-      };
-      console.log(newItem)
-      items.push(newItem)
-      console.log(items)
-
+  $.each(items, function () {
       
-  })
+      $(`<div id = item-box>
+          Id:          ${this.id}<br>
+          Name:        ${this.NAME}<br>
+          Price:       ${this.PRICE}<br><br>`
+      ).appendTo("#item-box");        
+  });
+
+  //USER STORY C1 - 15-05-2020
+
+    var newItem = {
+    id : 20,
+    NAME : $("#name").val(),
+    PRICE : $("#price").val(),
+    };
+
+    $("#addItem-button").click( () => {
+      let items = $("#item-box").length + 1;
+      $("#item-box").last().append("<li>Reason "+ newItem +" </li>");
+
+
+    console.log(newItem);
+    items.push(newItem);
+    console.log(items);
+  
+  });
 
 });
 
