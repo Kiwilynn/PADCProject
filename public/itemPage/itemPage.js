17b42487752a2c3a772b22ba486860feb238c493
MadsRF <madsrunefrederiksen@gmail.com>
2020-04-16 11:46:57 +0200

    C1 working but missing push to array

--- public/itemPage/itemPage.js
+++ public/itemPage/itemPage.js
@@ -2,42 +2,47 @@ $(document).ready( () => {
 
   // dummy data over items - C0 story - 15-05-2020
   let items = [
-      { id: "1", NAME: "Black Coffee", PRICE: 20},
-      { id: "2", NAME: "Cappuccino", PRICE: 25},
-      { id: "3", NAME: "Kaffe Latte", PRICE: 30},
-      { id: "4", NAME: "Hot Chocolate", PRICE: 38},
-      { id: "5", NAME: "Filter Kaffe", PRICE: 10},
-      { id: "6", NAME: "Water", PRICE: 1230}
-    ];
+    { id: "1", NAME: "Black Coffee", PRICE: 20},
+    { id: "2", NAME: "Cappuccino", PRICE: 25},
+    { id: "3", NAME: "Kaffe Latte", PRICE: 30},
+    { id: "4", NAME: "Hot Chocolate", PRICE: 38},
+    { id: "5", NAME: "Filter Kaffe", PRICE: 10},
+    { id: "6", NAME: "Water", PRICE: 1230}
+  ];
   
   
-  $.each(items, function () {
-      
-      $(`<div id = item-box>
-          Id:          ${this.id}<br>
-          Name:        ${this.NAME}<br>
-          Price:       ${this.PRICE}<br><br>`
-      ).appendTo("#item-box");        
+  $.each(items, function () {      
+    $(`<div id = item-box>
+      Id:          ${this.id}<br>
+      Name:        ${this.NAME}<br>
+      Price:       ${this.PRICE}<br><br>`
+    ).appendTo("#item-box");        
   });
 
   //USER STORY C1 - 15-05-2020
-
-    var newItem = {
-    id : 20,
-    NAME : $("#name").val(),
-    PRICE : $("#price").val(),
+  $("#addItem-button").click( () => {
+    
+    let newItem = {
+      id : 20,
+      NAME : $("#name").val(),
+      PRICE : $("#price").val(),
     };
-
-    $("#addItem-button").click( () => {
-      let items = $("#item-box").length + 1;
-      $("#item-box").last().append("<li>Reason "+ newItem +" </li>");
-
+    
+    let items = $("#item-box").length + 1;
+    $("#item-box").last().append(
+      `<div id = item-box>
+      Id:          ${newItem.id}<br>
+      Name:        ${newItem.NAME}<br>
+      Price:       ${newItem.PRICE}<br><br>`
+    );
 
     console.log(newItem);
     items.push(newItem);
     console.log(items);
-  
+    
+    
   });
+
 
 });
 
