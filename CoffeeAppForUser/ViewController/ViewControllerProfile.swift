937d23b0976f3b5e650cfd5086603cefa913fec4
Oliver Kramer <3520_kramer@live.dk>
2020-04-23 10:18:39 +0200

    Tab bar controller implemented

---
+++ CoffeeAppForUser/ViewController/ViewControllerProfile.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerProfile.swift
+//  CoffeeAppForUser
+//
+//  Created by Oliver Kramer on 23/04/2020.
+//  Copyright © 2020 Kea. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
